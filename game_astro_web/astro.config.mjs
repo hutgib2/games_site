0e51a64b82bcce7f245a1bf843f75afbb48e19d6
Hugo Smith <hugowsmith1301@gmail.com>
2026-06-11 18:29:51 +1000

    a

--- game_astro_web/astro.config.mjs
+++ game_astro_web/astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { webcore } from 'webcoreui/integration'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [webcore()]
+})
---
+++ game_astro_web/eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@eslint/js';
+import * as astro from 'eslint-plugin-astro';
+import ts from 'typescript-eslint';
+
+export default [
+	js.configs.recommended,
+	{
+		ignores: ['dist/', '.astro/', 'node_modules/', '**/*.config.{js,mjs,ts}'],
+	},
+	{
+		files: ['**/*.{ts,tsx,mts,cts}'],
+		extends: [...ts.configs.recommended],
+	},
+	{
+		files: ['**/*.astro'],
+		extends: [...astro.configs.recommended],
+	},
+];
