a6e124eeffdfbafae09c1c9e8b1b2bcaedc81d31
Hugo Smith <hugowsmith1301@gmail.com>
2026-06-16 17:34:11 +1000

    added logo and change website's name
    Co-authored-by: edenjamsalem <edenjamsalem@users.noreply.github.com>

--- game_astro_web/astro.config.mjs
+++ game_astro_web/astro.config.mjs
@@ -1,7 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import { webcore } from 'webcoreui/integration'
 
 export default defineConfig({
-    integrations: [webcore()]
+    integrations: [webcore()],
+    fonts: [
+		{
+			provider: fontProviders.local(),
+			name: 'Oswald',
+			cssVariable: '--font-oswald',
+            optimizedFallbacks: false,
+			options: {
+				variants: [
+					{
+						src: ['./src/assets/fonts/Oswald-Light.ttf'],
+						style: 'normal',
+						weight: 300,
+					},
+					{
+						src: ['./src/assets/fonts/Oswald-Regular.ttf'],
+						style: 'normal',
+						weight: 400,
+					},
+					{
+						src: ['./src/assets/fonts/Oswald-Medium.ttf'],
+						style: 'normal',
+						weight: 500,
+					},
+				],
+			},
+		},
+	],
 })
