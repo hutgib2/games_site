974c3f22e9571a2a2f2df20166bf98da62a59852
Hugo Smith <hugowsmith1301@gmail.com>
2026-06-17 18:28:33 +1000

    fixed carousel cards. Co-authored-by: edenjamsalem <edenjamsalem@users.noreply.github.com>

---
+++ game_astro_web/src/data/gameData.ts
@@ -0,0 +1,27 @@
+import ChessImage from '../assets/thumbnails/chess_preview.png';
+import TriviaImage from '../assets/thumbnails/trivia_preview.png';
+import ShooterImage from '../assets/thumbnails/shooter_preview.png';
+import SurvivorImage from '../assets/thumbnails/survivor_preview.png';
+
+export const gameData = {
+	chess: {	
+		title: 'Chess Reboot',
+		image: ChessImage,
+		description: 'This is some placeholder text.',
+	},
+    trivia: {	
+		title: 'Trivia',
+		image: TriviaImage,
+		description: 'This is some placeholder text.',
+	},
+    space_shooter: {	
+		title: 'Space Shooter',
+		image: ShooterImage,
+		description: 'This is some placeholder text.',
+	},
+    vampire_survivor: {	
+		title: 'Vampire Survivor',
+		image: SurvivorImage,
+		description: 'This is some placeholder text.',
+	},
+}
