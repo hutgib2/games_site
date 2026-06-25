90b6281ffe6d5437aa17f5242bb71f9a1cd7eb74
Hugo <hugowsmith13@gmail.com>
2026-06-25 18:31:05 +1000

    added a select bar and search bar Co-authored-by: edenjamsalem <edenjamsalem@users.noreply.github.com>

--- game_astro_web/src/lib/gameData.ts
+++ game_astro_web/src/lib/gameData.ts
@@ -1,27 +1,35 @@
 import ChessImage from '../assets/thumbnails/chess_preview.png';
-import TriviaImage from '../assets/thumbnails/trivia_preview.png';
 import ShooterImage from '../assets/thumbnails/shooter_preview.png';
 import SurvivorImage from '../assets/thumbnails/survivor_preview.png';
+import TriviaImage from '../assets/thumbnails/trivia_preview.png';
 
 export const gameData = {
 	chess: {	
 		title: 'Chess Reboot',
 		image: ChessImage,
 		description: 'This is some placeholder text.',
+		href: '/games/chess-reboot',
+		hrefSuffix: 'chess-reboot',
 	},
     trivia: {	
 		title: 'Trivia',
 		image: TriviaImage,
 		description: 'This is some placeholder text.',
+		href: '/games/trivia',
+		hrefSuffix: 'trivia',
 	},
     space_shooter: {	
 		title: 'Space Shooter',
 		image: ShooterImage,
 		description: 'This is some placeholder text.',
+		href: '/games/space-shooter',
+		hrefSuffix: 'space-shooter',
 	},
     vampire_survivor: {	
 		title: 'Vampire Survivor',
 		image: SurvivorImage,
 		description: 'This is some placeholder text.',
+		href: '/games/vampire-survivor',
+		hrefSuffix: 'vampire-survivor',
 	},
 }
