c858d0a4eec9b3c76ec99cd9409d1c8be8364d5f
Hugo <hugowsmith13@gmail.com>
2026-07-08 18:14:38 +1000

    added hover over gamecards
    Co-authored-by: edenjamsalem <edenjamsalem@users.noreply.github.com>

--- game_astro_web/eslint.config.js
+++ game_astro_web/eslint.config.js
@@ -1,18 +1,19 @@
+import { defineConfig } from 'eslint/config';
 import js from '@eslint/js';
 import * as astro from 'eslint-plugin-astro';
 import ts from 'typescript-eslint';
 
-export default [
-	js.configs.recommended,
-	{
-		ignores: ['dist/', '.astro/', 'node_modules/', '**/*.config.{js,mjs,ts}'],
-	},
-	{
-		files: ['**/*.{ts,tsx,mts,cts}'],
-		extends: [...ts.configs.recommended],
-	},
-	{
-		files: ['**/*.astro'],
-		extends: [...astro.configs.recommended],
-	},
-];
+export default defineConfig([
+  js.configs.recommended,
+  {
+    ignores: ['dist/', '.astro/', 'node_modules/', '**/*.config.{js,mjs,ts}'],
+  },
+  {
+    files: ['**/*.{ts,tsx,mts,cts}'],
+    extends: [...ts.configs.recommended],
+  },
+  {
+    files: ['**/*.astro'],
+    extends: [...astro.configs.recommended],
+  },
+]);
